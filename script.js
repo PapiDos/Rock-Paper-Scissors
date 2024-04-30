620b1346d4775ad4c7cf436493e95d40c450d543
PapiDos <35791533+PapiDos@users.noreply.github.com>
2024-04-30 13:37:36 -0400

    Create score variables and playRound function
    Create humanScore & computerScore variables to keep track of the scores.
    The playRound() function contains the logic to play a single round of
    the game and update the scores based on the outcome of the round

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ function getComputerChoice() {
   let computerChoice = ["rock", "paper", "scissors"];
   return computerChoice[Math.floor(Math.random() * 3)];
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+// FUNCTION TO PLAY A SINGLE ROUND OF THE GAME
+function playRound(humanChoice, computerChoice) {
+  // Determine round outcome and update scores accordingly.
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+  } else {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+  }
+}
