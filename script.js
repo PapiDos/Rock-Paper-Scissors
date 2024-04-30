05fb2c47aff894f8bc0c157cd62f262dff5d7e55
PapiDos <35791533+PapiDos@users.noreply.github.com>
2024-04-30 01:27:04 -0400

    Create getComputerChoice function
    This function will generate a random computer selection

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function getHumanChoice() {
   let playerChoice = prompt("Choose rock, paper or scissors");
   return playerChoice;
 }
+
+// Function to generate computer choice
+function getComputerChoice() {
+  let ComputerChoice = ["rock", "paper", "scissors"];
+  return ComputerChoice[Math.floor(Math.random() * 3)];
+}
