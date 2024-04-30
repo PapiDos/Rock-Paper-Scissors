0b270f0ef01291b252698b0173746f98abc01f3d
PapiDos <35791533+PapiDos@users.noreply.github.com>
2024-04-30 02:25:52 -0400

    Change variable name, add method to returned value
    Change variable name for naming consistency.
    Add toLowerCase() method to returned value to
    normalize user input for comparison

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Function to prompt for human choice
 function getHumanChoice() {
-  let playerChoice = prompt("Choose rock, paper or scissors");
-  return playerChoice;
+  let humanChoice = prompt("Choose rock, paper or scissors");
+  return humanChoice.toLowerCase(); // Normalize human choice for comparison
 }
 
 // Function to generate computer choice
